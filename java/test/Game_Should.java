b0925bea1f2ed6cdf01d16583decec88b81a09ed
droidsam1 <droidsam@gmail.com>
2022-12-14 19:35:53 +0100

    refactor: extract constant

--- java/test/Game_Should.java
+++ java/test/Game_Should.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Game_Should {
+    public static final char PLAYER_X = 'X';
     private Game game;
 
     @BeforeEach
@@ -20,15 +21,15 @@ public class Game_Should {
     @Test
     public void NotAllowPlayerXToPlayTwiceInARow() {
         assertThrows(Exception.class, () -> {
-            game.Play('X', 0, 0);
-            game.Play('X', 1, 0);
+            game.Play(PLAYER_X, 0, 0);
+            game.Play(PLAYER_X, 1, 0);
         });
     }
 
     @Test
     public void NotAllowPlayerToPlayInLastPlayedPosition() {
         assertThrows(Exception.class, () -> {
-            game.Play('X', 0, 0);
+            game.Play(PLAYER_X, 0, 0);
             game.Play('O', 0, 0);
         });
     }
@@ -36,32 +37,32 @@ public class Game_Should {
     @Test
     public void NotAllowPlayerToPlayInAnyPlayedPosition() {
         assertThrows(Exception.class, () -> {
-            game.Play('X', 0, 0);
+            game.Play(PLAYER_X, 0, 0);
             game.Play('O', 1, 0);
-            game.Play('X', 0, 0);
+            game.Play(PLAYER_X, 0, 0);
         });
     }
 
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInTopRow() throws Exception {
-        game.Play('X', 0, 0);
+        game.Play(PLAYER_X, 0, 0);
         game.Play('O', 1, 0);
-        game.Play('X', 0, 1);
+        game.Play(PLAYER_X, 0, 1);
         game.Play('O', 1, 1);
-        game.Play('X', 0, 2);
+        game.Play(PLAYER_X, 0, 2);
 
         char winner = game.Winner();
 
-        assertEquals('X', winner);
+        assertEquals(PLAYER_X, winner);
     }
 
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInTopRow() throws Exception {
-        game.Play('X', 2, 2);
+        game.Play(PLAYER_X, 2, 2);
         game.Play('O', 0, 0);
-        game.Play('X', 1, 0);
+        game.Play(PLAYER_X, 1, 0);
         game.Play('O', 0, 1);
-        game.Play('X', 1, 1);
+        game.Play(PLAYER_X, 1, 1);
         game.Play('O', 0, 2);
 
         char winner = game.Winner();
@@ -71,24 +72,24 @@ public class Game_Should {
 
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInMiddleRow() throws Exception {
-        game.Play('X', 1, 0);
+        game.Play(PLAYER_X, 1, 0);
         game.Play('O', 0, 0);
-        game.Play('X', 1, 1);
+        game.Play(PLAYER_X, 1, 1);
         game.Play('O', 0, 1);
-        game.Play('X', 1, 2);
+        game.Play(PLAYER_X, 1, 2);
 
         char winner = game.Winner();
 
-        assertEquals('X', winner);
+        assertEquals(PLAYER_X, winner);
     }
 
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInMiddleRow() throws Exception {
-        game.Play('X', 0, 0);
+        game.Play(PLAYER_X, 0, 0);
         game.Play('O', 1, 0);
-        game.Play('X', 2, 0);
+        game.Play(PLAYER_X, 2, 0);
         game.Play('O', 1, 1);
-        game.Play('X', 2, 1);
+        game.Play(PLAYER_X, 2, 1);
         game.Play('O', 1, 2);
 
         char winner = game.Winner();
@@ -98,24 +99,24 @@ public class Game_Should {
 
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInBottomRow() throws Exception {
-        game.Play('X', 2, 0);
+        game.Play(PLAYER_X, 2, 0);
         game.Play('O', 0, 0);
-        game.Play('X', 2, 1);
+        game.Play(PLAYER_X, 2, 1);
         game.Play('O', 0, 1);
-        game.Play('X', 2, 2);
+        game.Play(PLAYER_X, 2, 2);
 
         char winner = game.Winner();
 
-        assertEquals('X', winner);
+        assertEquals(PLAYER_X, winner);
     }
 
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInBottomRow() throws Exception {
-        game.Play('X', 0, 0);
+        game.Play(PLAYER_X, 0, 0);
         game.Play('O', 2, 0);
-        game.Play('X', 1, 0);
+        game.Play(PLAYER_X, 1, 0);
         game.Play('O', 2, 1);
-        game.Play('X', 1, 1);
+        game.Play(PLAYER_X, 1, 1);
         game.Play('O', 2, 2);
 
         char winner = game.Winner();
