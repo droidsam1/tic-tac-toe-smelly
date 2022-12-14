0600776ef0007df000a45aa348e8466790dc9b82
droidsam1 <droidsam@gmail.com>
2022-12-14 19:03:14 +0100

    refactor: extract constant

--- java/src/Game.java
+++ java/src/Game.java
@@ -1,10 +1,11 @@
 public class Game {
+    public static final char NO_PLAYER_SYMBOL = ' ';
     private final Board board = new Board();
-    private char lastPlayedSymbol = ' ';
+    private char lastPlayedSymbol = NO_PLAYER_SYMBOL;
 
     public void Play(char symbol, int x, int y) throws Exception {
         //if first move
-        if (lastPlayedSymbol == ' ') {
+        if (lastPlayedSymbol == NO_PLAYER_SYMBOL) {
             //if player is X
             if (symbol == 'O') {
                 throw new Exception("Invalid first player");
@@ -15,7 +16,7 @@ public class Game {
             throw new Exception("Invalid next player");
         }
         //if not first move but play on an already played tile
-        else if (board.TileAt(x, y).Symbol != ' ') {
+        else if (board.TileAt(x, y).Symbol != NO_PLAYER_SYMBOL) {
             throw new Exception("Invalid position");
         }
 
@@ -26,7 +27,7 @@ public class Game {
 
     public char Winner() {
         //if the positions in first row are taken
-        if (board.TileAt(0, 0).Symbol != ' ' && board.TileAt(0, 1).Symbol != ' ' && board.TileAt(0, 2).Symbol != ' ') {
+        if (board.TileAt(0, 0).Symbol != NO_PLAYER_SYMBOL && board.TileAt(0, 1).Symbol != NO_PLAYER_SYMBOL && board.TileAt(0, 2).Symbol != NO_PLAYER_SYMBOL) {
             //if first row is full with same symbol
             if (board.TileAt(0, 0).Symbol == board.TileAt(0, 1).Symbol && board.TileAt(0, 2).Symbol == board.TileAt(0, 1).Symbol) {
                 return board.TileAt(0, 0).Symbol;
@@ -34,7 +35,7 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (board.TileAt(1, 0).Symbol != ' ' && board.TileAt(1, 1).Symbol != ' ' && board.TileAt(1, 2).Symbol != ' ') {
+        if (board.TileAt(1, 0).Symbol != NO_PLAYER_SYMBOL && board.TileAt(1, 1).Symbol != NO_PLAYER_SYMBOL && board.TileAt(1, 2).Symbol != NO_PLAYER_SYMBOL) {
             //if middle row is full with same symbol
             if (board.TileAt(1, 0).Symbol == board.TileAt(1, 1).Symbol && board.TileAt(1, 2).Symbol == board.TileAt(1, 1).Symbol) {
                 return board.TileAt(1, 0).Symbol;
@@ -42,14 +43,14 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (board.TileAt(2, 0).Symbol != ' ' && board.TileAt(2, 1).Symbol != ' ' && board.TileAt(2, 2).Symbol != ' ') {
+        if (board.TileAt(2, 0).Symbol != NO_PLAYER_SYMBOL && board.TileAt(2, 1).Symbol != NO_PLAYER_SYMBOL && board.TileAt(2, 2).Symbol != NO_PLAYER_SYMBOL) {
             //if middle row is full with same symbol
             if (board.TileAt(2, 0).Symbol == board.TileAt(2, 1).Symbol && board.TileAt(2, 2).Symbol == board.TileAt(2, 1).Symbol) {
                 return board.TileAt(2, 0).Symbol;
             }
         }
 
-        return ' ';
+        return NO_PLAYER_SYMBOL;
     }
 }
 
