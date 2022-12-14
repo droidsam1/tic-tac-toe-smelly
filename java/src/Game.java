9382b4f0fc268b68dd540e003effcd259b891d10
droidsam1 <droidsam@gmail.com>
2022-12-14 18:57:57 +0100

    fix code style. add .iml

--- java/src/Game.java
+++ java/src/Game.java
@@ -1,6 +1,6 @@
 public class Game {
     private char _lastSymbol = ' ';
-    private Board _board = new Board();
+    private final Board _board = new Board();
 
     public void Play(char symbol, int x, int y) throws Exception {
         //if first move
@@ -26,39 +26,25 @@ public class Game {
 
     public char Winner() {
         //if the positions in first row are taken
-        if (_board.TileAt(0, 0).Symbol != ' ' &&
-                _board.TileAt(0, 1).Symbol != ' ' &&
-                _board.TileAt(0, 2).Symbol != ' ') {
+        if (_board.TileAt(0, 0).Symbol != ' ' && _board.TileAt(0, 1).Symbol != ' ' && _board.TileAt(0, 2).Symbol != ' ') {
             //if first row is full with same symbol
-            if (_board.TileAt(0, 0).Symbol ==
-                    _board.TileAt(0, 1).Symbol &&
-                    _board.TileAt(0, 2).Symbol == _board.TileAt(0, 1).Symbol) {
+            if (_board.TileAt(0, 0).Symbol == _board.TileAt(0, 1).Symbol && _board.TileAt(0, 2).Symbol == _board.TileAt(0, 1).Symbol) {
                 return _board.TileAt(0, 0).Symbol;
             }
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ') {
+        if (_board.TileAt(1, 0).Symbol != ' ' && _board.TileAt(1, 1).Symbol != ' ' && _board.TileAt(1, 2).Symbol != ' ') {
             //if middle row is full with same symbol
-            if (_board.TileAt(1, 0).Symbol ==
-                    _board.TileAt(1, 1).Symbol &&
-                    _board.TileAt(1, 2).Symbol ==
-                            _board.TileAt(1, 1).Symbol) {
+            if (_board.TileAt(1, 0).Symbol == _board.TileAt(1, 1).Symbol && _board.TileAt(1, 2).Symbol == _board.TileAt(1, 1).Symbol) {
                 return _board.TileAt(1, 0).Symbol;
             }
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ') {
+        if (_board.TileAt(2, 0).Symbol != ' ' && _board.TileAt(2, 1).Symbol != ' ' && _board.TileAt(2, 2).Symbol != ' ') {
             //if middle row is full with same symbol
-            if (_board.TileAt(2, 0).Symbol ==
-                    _board.TileAt(2, 1).Symbol &&
-                    _board.TileAt(2, 2).Symbol ==
-                            _board.TileAt(2, 1).Symbol) {
+            if (_board.TileAt(2, 0).Symbol == _board.TileAt(2, 1).Symbol && _board.TileAt(2, 2).Symbol == _board.TileAt(2, 1).Symbol) {
                 return _board.TileAt(2, 0).Symbol;
             }
         }
