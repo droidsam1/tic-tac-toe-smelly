dde19576ef039399114638f8a300931e6e048e94
droidsam1 <droidsam@gmail.com>
2022-12-16 19:07:05 +0100

    refactor: extract method

--- java/src/Board.java
+++ java/src/Board.java
@@ -5,6 +5,10 @@ public class Board {
     private final List<Tile> tiles = new ArrayList<>();
 
     public Board() {
+        buildSquareBoard();
+    }
+
+    private void buildSquareBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 Tile tile = new Tile();
